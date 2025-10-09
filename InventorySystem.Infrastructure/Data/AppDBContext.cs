e06e045a4a0bcc016ca076830ccbc6661b91ffd7
abdoelfeky1 <abdoelfeky9@gmail.com>
2025-10-10 01:42:03 +0300

    Add Three Tables in DB

--- InventorySystem.Infrastructure/Data/AppDBContext.cs
+++ InventorySystem.Infrastructure/Data/AppDBContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InventorySystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace InventorySystem.Infrastructure.Data
 {
-    internal class AppDBContext
+    public class AppDBContext : DbContext
     {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<SaleDetails> SaleDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
