6fe6e2f0c553f9fc5914236f5362cc53636d46aa
A7medabdelaty <ahmed.abdelaty174@gmail.com>
2025-10-10 10:07:29 +0300

    refactor(entities): Update configurations and properties
    * Add `Configure` method to define `User` entity configuration.
    
    * Add `CustomerConfiguration` to configure `Customer` entity.
    
    * Add `PurchaseConfiguration` to configure `Purchase` entity.

--- InventorySystem.Core/Entities/Customer.cs
+++ InventorySystem.Core/Entities/Customer.cs
@@ -2,7 +2,7 @@
 {
     public class Customer
     {
-        public int Id { get; set; }
+        public int CustomerId { get; set; }
 
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
