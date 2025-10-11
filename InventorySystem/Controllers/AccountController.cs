9e9ce38d46f75d8c1518dde8c207fde0aa9d1886
Alaa <dralaaerfan@gmail.com>
2025-10-11 21:00:32 +0300

    feat: implement authentication system with login page
    - Add AccountController with login functionality
    - Create LoginViewModel with email/password/remember me
    - Implement login view with form validation
    - Setup ASP.NET Identity integration
    - Add access denied page and error handling
    - Configure authentication services in Program.cs

--- InventorySystem/Controllers/AccountController.cs
+++ InventorySystem/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using InventorySystem.web.View_Models;
+﻿using InventorySystem.Core.Entities;
+using InventorySystem.web.View_Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InventorySystem.web.Controllers
@@ -6,12 +8,16 @@ namespace InventorySystem.web.Controllers
     [AutoValidateAntiforgeryToken]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
 
-        public AccountController()
+        public AccountController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager)
         {
-            
+            this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
+        public UserManager<ApplicationUser> UserManager { get; }
 
         [HttpGet]
         public IActionResult Login()
@@ -20,11 +26,37 @@ namespace InventorySystem.web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel loginViewModel)
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
         {
+            if(ModelState.IsValid)
+            {
+                ApplicationUser user = await userManager.FindByEmailAsync(loginViewModel.Email);
+                if (user != null)
+                {
+                    bool found = await userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                    if (found) 
+                    {
+                        await signInManager.SignInAsync(user, loginViewModel.RememberMe);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                ModelState.AddModelError("", "Invalid Username or Password");
 
-            return View();
+
+            }
+            return View("Login",loginViewModel);
         }
 
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
