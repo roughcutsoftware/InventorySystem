8e3dbbd125f37ce33ec69492949965ce94d976bb
A7medabdelaty <ahmed.abdelaty174@gmail.com>
2025-10-16 19:16:08 +0300

    feat(supplier): add full MVC support for Supplier module
    - Implemented SupplierController with CRUD actions using SupplierService
    - Added Razor Views (Index, Create, Edit, Details, Delete)
    - Integrated logging via ILogService inside service layer
    - Bound forms to SupplierDto with validation and IsActive toggle

---
+++ InventorySystem/Views/Supplier/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventorySystem.web.Views.Supplier
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
